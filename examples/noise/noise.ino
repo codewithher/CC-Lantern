395baa33611beb8c87c9a1ef787f6715951761e5
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-07-02 23:01:48 -0700

    display perlin noise example with lantern

--- examples/noise/noise.ino
+++ examples/noise/noise.ino
@@ -7,14 +7,34 @@ Lantern lantern;
 void setup() {
     Serial.begin(9600);
     lantern.begin();
+    lantern.setBrightness(5);
 }
 
 void loop() {
+    generateNoiseColor();
+}
+
+void generateNoise() {
     for (float x = 0; x < 10; x += 0.1) {
         for (float y = 0; y < 10; y += 0.1) {
             float value = PerlinNoise::perlinNoise(x, y);
             Serial.println(value);
         }
     }
-    delay(1000);
+}
+
+void generateNoiseColor() {
+    for (float x = 0; x < 10; x += 0.1) {
+        for (float y = 0; y < 10; y += 0.1) {
+            const int SIGDIGITS = 1000;
+            float value = PerlinNoise::perlinNoise(x, y)*SIGDIGITS;
+            uint8_t color = map(value, -1*SIGDIGITS, 1*SIGDIGITS, 0, 255);
+            Serial.print("Color: ");
+            Serial.print(color);
+            Serial.print(" Value: ");
+            Serial.println(value);
+            lantern.setColor(0.1, lantern.colorWheel(color));
+        }
+    }
+    
 }
