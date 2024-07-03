b8289df0a8911caa88cfaaa8e6c82dc4c4fbfbd9
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-07-03 12:53:19 -0700

    cleaning up examples to remove comments

--- examples/noise/noise.ino
+++ examples/noise/noise.ino
@@ -4,6 +4,7 @@
 
 Lantern lantern;
 
+// Creating values outside so that the loop doesn't have to recalculate them
 uint8_t red = 255;
 uint8_t green = 0;
 uint8_t blue = 0;
@@ -55,10 +56,9 @@ void generateNoiseColor() {
 }
 
 /**
- * @brief Creates a fire effect using perlin noise. Limits rainbow effect 
- * to be within 1/6 of the color wheel, which is red -> yellow.
+ * @brief Creates a fire effect using perlin noise. Limits the fire effect
+ * to changing just the green channel of the rgb color.
  * 
- * https://en.wikipedia.org/wiki/Color_wheel
  */
 void fireNoise() {
     lantern.setColor(0.1, lantern.color(red, green, blue));
@@ -72,25 +72,6 @@ void fireNoise() {
             Serial.print("Green: ");
             Serial.println(green);
         }
-        // green  = PerlinNoise::perlinNoise(xoff, 0) * 255;
-        // xoff += inc;
     }
     start += inc;
-
-    // // perlinNoise produces a number between -1 and 1.
-    // // map takes in integers, so we multiply noise by 1000 to get more precision.
-    // const int SCALAR = 5;
-    // uint8_t green = 0;
-    // for (float x = 0; x < 100; x += 0.1) {
-    //     for (float y = 0; y < 100; y += 0.1) {
-    //         float value = PerlinNoise::perlinNoise(x, y) * SCALAR;
-    //         // green += round(value);
-    //         uint8_t green = map(value, -1 * SCALAR, 1 * SCALAR, 0, 150);
-    //         Serial.print("Green: ");
-    //         Serial.print(green);
-    //         Serial.print(" Value: ");
-    //         Serial.println(value);
-    //         lantern.setColor(0.01, lantern.color(255, green, 0));
-    //     }
-    // }
 }
